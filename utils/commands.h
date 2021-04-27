ba8ea58ab9845c969c12e9da8e3db48304db6dfb
yaishenka <gagarinovdaniil@gmail.com>
2021-04-27 04:54:44 +0300

    fix command enum

--- utils/commands.h
+++ utils/commands.h
@@ -8,22 +8,7 @@
 
 #include <stdint.h>
 
-//#define HELP "help"
-//#define LS "ls"
-//#define INIT "init"
-//#define READ_FS "read_fs"
-//#define MKDIR "mkdir"
-//#define TOUCH "touch"
-//#define OPEN "open"
-//#define QUIT "quit"
-//#define CLOSE "close"
-//#define WRITE "write"
-//#define WRITE_FROM "write_from"
-//#define READ "read"
-//#define READ_TO "read_to"
-//#define LSEEK "lseek"
-
-enum Command: uint8_t {
+enum Command {
   Help = 1,
   Ls,
   Init,
